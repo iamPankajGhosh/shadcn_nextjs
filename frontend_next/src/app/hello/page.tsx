e5b3da34800e4e1bab6aa37a9eebbcbcc4f7fbe6
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-01-30 00:37:38 +0530

    first next app done

---
+++ frontend_next/src/app/hello/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function page() {
+  return (
+    <main className="h-full flex items-center justify-center flex-col">
+      <div>hello world</div>
+      <Button variant="outline">ShadCN Button</Button>
+    </main>
+  )
+}
+
+export default page
